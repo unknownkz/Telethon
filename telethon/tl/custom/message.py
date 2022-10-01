2693d6bb6e09bfa083689511883f066db6d7ced0
unknownkz <unknownkz@outlook.co.id>
2022-10-01 15:53:19 +0700

    .
    .
    
    Signed-off-by: unknownkz <unknownkz@outlook.co.id>

--- telethon/tl/custom/message.py
+++ telethon/tl/custom/message.py
@@ -680,24 +680,24 @@ class Message(ChatGetter, SenderGetter, TLObject):
 
     @property
     def link_message(self):
-    if (
-        hasattr(self.chat, 'username')
-        and self.chat.username
-    ):
-        return (
-            f'https://t.me/{self.chat.username}/{self.id}'
-        )
-        if self.chat and self.chat.id:
-            chat = self.chat.id
-        elif self.chat_id:
-            if str(self.chat_id).startswith('-' or '-100'):
-                chat = int(
-                    str(self.chat_id)
-                    .replace('-100', '')
-                    .replace('-', '')
-                )
-            else:
-                chat = self.chat_id
+        if (
+            hasattr(self.chat, 'username')
+            and self.chat.username
+        ):
+            return (
+                f'https://t.me/{self.chat.username}/{self.id}'
+            )
+            if self.chat and self.chat.id:
+                chat = self.chat.id
+            elif self.chat_id:
+                if str(self.chat_id).startswith('-' or '-100'):
+                    chat = int(
+                        str(self.chat_id)
+                        .replace('-100', '')
+                        .replace('-', '')
+                    )
+                else:
+                    chat = self.chat_id
         else:
             return
         return f'https://t.me/c/{chat}/{self.id}'
