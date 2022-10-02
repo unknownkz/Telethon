2058b769c5532c91e76230665ed3885debbe1e8a
unknownkz <unknownkz@outlook.co.id>
2022-10-02 18:53:13 +0700

    .
    .
    
    Signed-off-by: unknownkz <unknownkz@outlook.co.id>

--- telethon/tl/custom/message.py
+++ telethon/tl/custom/message.py
@@ -680,30 +680,6 @@ class Message(ChatGetter, SenderGetter, TLObject):
 
     # endregion Public Properties
 
-    @property
-    def link_message(self):
-        if (
-            hasattr(self.chat, 'username')
-            and self.chat.username
-        ):
-            return (
-                f'https://t.me/{self.chat.username}/{self.id}'
-            )
-            if self.chat and self.chat.id:
-                chat = self.chat.id
-            elif self.chat_id:
-                if str(self.chat_id).startswith('-' or '-100'):
-                    chat = int(
-                        str(self.chat_id)
-                        .replace('-100', '')
-                        .replace('-', '')
-                    )
-                else:
-                    chat = self.chat_id
-            else:
-                return
-            return f'https://t.me/c/{chat}/{self.id}'
-
     # region Public Methods
 
     def get_entities_text(self, cls=None):
