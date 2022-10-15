881e7f638e14cd9162a64598496e83429bceea70
unknownkz <unknownkz@outlook.co.id>
2022-10-15 19:01:29 +0700

    .
    .
    
    Signed-off-by: unknownkz <unknownkz@outlook.co.id>

--- telethon/tl/custom/message.py
+++ telethon/tl/custom/message.py
@@ -685,6 +685,32 @@ class Message(ChatGetter, SenderGetter, TLObject):
 
         return self.peer_id
 
+    @property
+    def link_message(self):
+        if (
+            hasattr(self.chat, 'username')
+            and self.chat.username
+        ):
+            return (
+                f'https://t.me/{self.chat.username}/{self.id}'
+            )
+            if self.chat and self.chat.id:
+                chat = self.chat.id
+            elif self.chat_id:
+                if str(self.chat_id).startswith('-' or '-100'):
+                    chat = int(
+                        str(self.chat_id)
+                        .replace('-100', '')
+                        .replace('-', '')
+                    )
+                else:
+                    chat = self.chat_id
+            else:
+                return None
+            if self.is_private:
+                return f'tg://openmessage?user_id={chat}&message_id={self.id}'
+            return f'https://t.me/c/{chat}/{self.id}'
+
     # endregion Public Properties
 
     # region Public Methods
