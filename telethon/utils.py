0b8d00d4e3ca86f99a1a0d9ad52249289a78ddd9
unknownkz <unknownkz@outlook.co.id>
2022-10-19 23:48:17 +0700

    .
    .
    
    Signed-off-by: unknownkz <unknownkz@outlook.co.id>

--- telethon/utils.py
+++ telethon/utils.py
@@ -87,23 +87,27 @@ def link_message(self):
         return (
             f'https://t.me/{self.chat.username}/{self.id}'
         )
-        if self.chat and self.chat.id:
-            chat = self.chat.id
-        elif self.chat_id:
-            if str(self.chat_id).startswith('-' or '-100'):
-                chat = int(
-                    str(self.chat_id)
-                    .replace('-100', '')
-                    .replace('-', '')
-                )
-            else:
-                chat = self.chat_id
+    if self.chat and self.chat.id:
+        chat = self.chat.id
+    elif self.chat_id:
+        if str(self.chat_id).startswith('-' or '-100'):
+            chat = int(
+                str(self.chat_id)
+                .replace('-100', '')
+                .replace('-', '')
+            )
         else:
-            return None
-        if self.is_private:
+            chat = self.chat_id
+    else:
+        return None
+
+    if self.is_private:
+        if not self.is_group:
             return f'tg://openmessage?user_id={chat}&message_id={self.id}'
 
         return f'https://t.me/c/{chat}/{self.id}'
+
+    return f'https://t.me/c/{chat}/{self.id}'
 
 
 def get_msg_id(
