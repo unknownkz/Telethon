5b833a8e59f91675cb236a845f63978c90ee29c2
unknownkz <unknownkz@outlook.co.id>
2022-10-31 01:25:06 +0700

    test custom markdown
    Signed-off-by: unknownkz <unknownkz@outlook.co.id>

---
+++ telethon/partcustom.py
@@ -0,0 +1,18 @@
+from .extensions import markdown
+from .tl import types
+
+
+class CustomMarkdown:
+    @staticmethod
+    def parse(text):
+        text, entities = markdown.parse(text)
+        for i, e in enumerate(entities):
+            if isinstance(e, types.MessageEntityTextUrl):
+                if e.url == 'spoiler':
+                    entities[i] = types.MessageEntitySpoiler(e.offset, e.length)
+                elif e.url.startswith('emoji/'):
+                    entities[i] = types.MessageEntityCustomEmoji(e.offset, e.length, int(e.url.split('/')[1]))
+        return text, entities
+    @staticmethod
+    def unparse(text, entities):
+        return markdown.unparse(text, entities)
